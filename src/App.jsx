b7c1d1bd5e9cd36c1a08b8b1407454edf2c772c6
steelsnake93 <nikolanikotasev@protonmail.com>
2023-12-14 12:25:15 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import 'normalize.css';
+import "./components/assets/styles/global.css";
+
+export default function App() {
+    return (
+        <>
+        <h1>Hello</h1>
+        </>
+    );
+}
